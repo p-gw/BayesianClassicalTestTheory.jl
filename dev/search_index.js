a760e93b6ddeed3b0039e64434f8a35cd6d83fb0
Documenter.jl <documenter@juliadocs.github.io>
2023-02-24 13:00:18 +0000

    build based on 6685e58

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BayesianClassicalTestTheory","category":"page"},{"location":"#BayesianClassicalTestTheory","page":"Home","title":"BayesianClassicalTestTheory","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BayesianClassicalTestTheory.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BayesianClassicalTestTheory]","category":"page"}]
+}
